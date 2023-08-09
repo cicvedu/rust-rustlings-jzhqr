0f8642ab2f7f2a7fb57543f1e7d1a677880aded6
jzhqr <jzhqr@163.com>
2023-08-09 17:03:18 +0800

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// I AM  DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(x:i32) -> i32 {
+    if x>40 {
+        x
+    } else {
+        2*x
+    }
+
+ }
 
 // Don't modify this function!
 #[test]
